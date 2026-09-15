a8c510bf8288c199f0deabd04486b11092db7d0d
marketbridgefarm-cmyk <marketbridgefarm@gmail.com>
2026-09-15 13:09:36 +0300

    Add files via upload

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,14 +4,17 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
+import { I18nProvider } from './i18n/I18nContext.jsx';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <I18nProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </I18nProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
