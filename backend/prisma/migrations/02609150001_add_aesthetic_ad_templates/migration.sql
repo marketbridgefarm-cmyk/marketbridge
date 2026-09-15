796ef53d42c50c678161f45edf8e349b6d43a7b2
marketbridgefarm-cmyk <marketbridgefarm@gmail.com>
2026-09-15 23:34:32 +0300

    Delete backend/prisma/migrations/02609150001_add_aesthetic_ad_templates directory

--- backend/prisma/migrations/02609150001_add_aesthetic_ad_templates/migration.sql
+++
@@ -1,7 +0,0 @@
--- Extend banner template enum for the new premium/aesthetic ad layouts.
-ALTER TYPE "BannerTemplate" ADD VALUE IF NOT EXISTS 'SPLIT';
-ALTER TYPE "BannerTemplate" ADD VALUE IF NOT EXISTS 'EDITORIAL';
-ALTER TYPE "BannerTemplate" ADD VALUE IF NOT EXISTS 'FRESH';
-ALTER TYPE "BannerTemplate" ADD VALUE IF NOT EXISTS 'DARK_LUXE';
-ALTER TYPE "BannerTemplate" ADD VALUE IF NOT EXISTS 'MARKET';
-ALTER TYPE "BannerTemplate" ADD VALUE IF NOT EXISTS 'GRADIENT';
