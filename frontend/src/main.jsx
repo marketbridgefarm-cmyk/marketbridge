07c4a81c65e9dedd6ae3dade6a5e72e3bb3f1ef2
marketbridgefarm-cmyk <marketbridgefarm@gmail.com>
2026-09-19 22:03:36 +0300

    Add files via upload

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
+import ErrorBoundary from './components/ErrorBoundary.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
 import { I18nProvider } from './context/I18nContext.jsx';
 import { ToastProvider } from './context/ToastContext.jsx';
@@ -11,14 +12,16 @@ import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <I18nProvider>
-        <AuthProvider>
-          <ToastProvider>
-            <App />
-          </ToastProvider>
-        </AuthProvider>
-      </I18nProvider>
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <I18nProvider>
+          <AuthProvider>
+            <ToastProvider>
+              <App />
+            </ToastProvider>
+          </AuthProvider>
+        </I18nProvider>
+      </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>
 );
